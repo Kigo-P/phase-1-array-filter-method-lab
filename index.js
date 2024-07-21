a548a3280c944018762192088430d4160718f596
Kigo-P <peterkigo33@gmail.com>
2024-07-21 15:40:42 +0300

    last Commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers,name){
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase().includes(name.toLowerCase());
+    });
+}
+console.log(findMatching("bobby"))
+
+function fuzzyMatch(drivers,letters){
+    return drivers.filter(function(driver) {
+        return driver.startsWith(letters)})
+}
+
+function matchName(drivers,name){
+    return drivers.filter(function(driver) {
+        return driver.name === name;
+    });
+}
+
